eb98346bbdf957b4855a8845e92e86e4418316fe
Mohamed Shaw <mohamedShawD08@gmail.com>
2023-07-27 13:56:32 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+</template>
+
+<script>
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+import axios from "axios";
+
+const { data: token } = await axios.post("http://127.0.0.1:8000/api/sanctum/token", {
+  email: "user@example.app",
+  password: "password",
+})
+
+let echo = new Echo({
+  broadcaster: "pusher",
+  key: "chat-app-key",
+  wsHost: "127.0.0.1",
+  wsPort: 6001,
+  forceTLS: false,
+  cluster: "mt1",
+  disableStats: true,
+  authorizer: (channel, options) => {
+    // console.log(options);
+    return {
+      authorize: (socketId, callback) => {
+        axios({
+          method: "POST",
+          url: "http://127.0.0.1:8000/api/broadcasting/auth",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          data: {
+            socket_id: socketId,
+            channel_name: channel.name,
+          },
+        })
+          .then((response) => {
+            callback(false, response.data);
+          })
+          .catch((error) => {
+            callback(true, error);
+          });
+      },
+    };
+  },
+});
+
+echo.private('App.User.2')
+  .listen(".chat-message", (message) => console.log(message));
+
+echo.join(`presence.chat.1`)
+  .here((users) => console.log({ users: users }))
+  .listen(".message", (message) => console.log(message));
+
+// const response = await axios.post('http://127.0.0.1:8000/api/event', {
+//   message: 'What it is?'
+// });
+
+// console.log(response);
+</script>
